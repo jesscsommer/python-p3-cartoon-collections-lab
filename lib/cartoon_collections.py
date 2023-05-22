01227a3b3ef4b8945a9b3bd666e647fa3ffb0e42
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-22 14:47:36 -0700

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f'{i + 1}. {names[i]}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    new_calls = [call.capitalize() + "!" for call in calls]
+    return new_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls: 
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strs):
+    for str in strs: 
+        if str in ['cheddar', 'gouda', 'camembert']:
+            return str 
+    return None
